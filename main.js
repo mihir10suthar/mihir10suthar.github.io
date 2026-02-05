bfdd6d17fa18cce74764f3ce546724c45e184173
mihir10suthar <mihir10suthar@gmail.com>
2026-02-05 15:17:43 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function login(){
-  if(user.value.toLowerCase()==="ankita" && pass.value==="16091010"){
-    localStorage.setItem("music","on");
-    location.href="unlock.html";
-  }else{
-    loginBox.classList.add("shake");
-    setTimeout(()=>loginBox.classList.remove("shake"),400);
-  }
-}
